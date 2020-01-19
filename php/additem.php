7cec5b0f9d2131ec433696a4de1d9234ef3d4f6f
Suvink <hello@suvink.me>
2020-01-19 23:59:24 +0530

    Add item php

--- php/additem.php
+++ php/additem.php
@@ -1,10 +1,9 @@
 <?php
-    
+    require_once 'db.php';
     session_start();
     $customer_id = $_SESSION['userid'];
 
     if ( isset( $_POST['submit'] ) ){
-        
     $customer_name = $_REQUEST['cname'];
     $event_date = $_REQUEST['cdate'];
     $event_venue = $_REQUEST['cvenue'];
@@ -12,28 +11,17 @@
     $event_package = $_REQUEST['cpackage'];
     $additional_details = $_REQUEST['cadditional'];
     
-    //Database Connection details  
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "partymania";
-    // Create connection
-    $con = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    } 
 
     $sql=  "INSERT INTO  (customerid, name, date, venue, participants, package, remarks) 
                 VALUES ('$customer_id', '$customer_name', '$event_date', '$event_venue', '$event_participants', '$event_package', '$additional_details')";
     }
-    if ($conn->query($sql) === TRUE) {
+    if ($con->query($sql) === TRUE) {
         echo("Done");
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . "<br>" . $con->error;
     }
 
-    $conn->close();
+    $con->close();
 
 
 
