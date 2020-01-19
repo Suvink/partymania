f35f5770b48f353168be454b8584a204364f5c78
Suvink <hello@suvink.me>
2020-01-19 23:28:43 +0530

    add page completed and db con seperated

---
+++ php/db.php
@@ -0,0 +1,12 @@
+<?php
+//Database Connection details  
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "partymania";
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
