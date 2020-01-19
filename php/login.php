4aaa40493f5a9466686d0caa098beeba73d5b127
Suvink <hello@suvink.me>
2020-01-19 23:30:20 +0530

    db con seperated

--- php/login.php
+++ php/login.php
@@ -1,18 +1,9 @@
 <?php
+require_once 'db.php';
 session_start();
 //Login
 if ( isset( $_POST['submit'] ) ){
-    //Database Connection details  
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "partymania";
-    // Create connection
-    $con = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    } 
+
     $email = $_REQUEST['email'];
     $pw = $_REQUEST['pw'];
     echo $email;
--- php/signup.php
+++ php/signup.php
@@ -1,20 +1,10 @@
 <?php
-
+require_once 'db.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $pw = $_POST['pw'];
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "partymania";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
     $sql=  "INSERT INTO users (name, email, phone, password) 
             VALUES ('$name', '$email', '$phone', '$pw')";
     if ($conn->query($sql) === TRUE) {
