00bff19e35bf23f634af23054799bdb515deae40
Suvink <hello@suvink.me>
2020-01-20 16:25:16 +0530

    Minor stylling

--- dashboard.php
+++ dashboard.php
@@ -54,18 +54,21 @@ if(!isset($_SESSION['userid'])){
                 <h4 style="margin:0">Birthday Cake</h4>
                 <h4 style="margin:0">party Props</h4>
                 <h4 style="margin:0">Invitations</h4>
+                <h2 style="margin-bottom:10px;margin-top: 20px color:#f56;">LKR 50,000.00</h2>
             </div>
             <div class="overall-card">
                 <h2 style="margin-bottom:10px">Wedding package</h2>
                 <h4 style="margin:0">Wedding Cake</h4>
                 <h4 style="margin:0">Photography</h4>
                 <h4 style="margin:0">Decorations</h4>
+                <h2 style="margin-bottom:10px;margin-top: 20px color:#f56;">LKR 200,000.00</h2>
             </div>
             <div class="overall-card">
                 <h2 style="margin-bottom:10px">Funeral package</h2>
                 <h4 style="margin:0">Coffin</h4>
                 <h4 style="margin:0">Photography</h4>
                 <h4 style="margin:0">Decorations</h4>
+                <h2 style="margin-bottom:10px;margin-top: 20px color:#f56;">LKR 75,000.00</h2>
             </div>
         </div>
     </section>
@@ -88,7 +91,7 @@ if(!isset($_SESSION['userid'])){
                         echo '</center><div class="row" style="margin-top: 5rem;">
                                 <center>
                                     <div class="row">
-                                        <h3>Track your order</h3>
+                                        <h3>Your Order Details</h3>
                                         <img src="img/loader.gif" width="100px">
                                         <br>
                                         <div class="timeline-item" style="background-color: #f56;">
