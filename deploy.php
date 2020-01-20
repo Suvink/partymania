21b70c0522cc96b543acd55236c57670b2ad720b
Suvink <hello@suvink.me>
2020-01-20 14:57:19 +0530

    Deploy Scripts

--- deploy.php
+++ deploy.php
@@ -3,6 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec(‘sudo su && cd /jet/www/default/partymania && git reset –hard HEAD && git pull’);
+    shell_exec('sudo su');
+    shell_exec(‘cd /jet/www/default/partymania && git reset –hard HEAD && git pull’);
 }
 ?>hi
