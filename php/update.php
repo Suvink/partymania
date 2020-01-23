475c44992388bc7a988d033550b9d74126a93f89
Suvink <hello@suvink.me>
2020-01-23 18:11:27 +0530

    Admin Table

--- php/update.php
+++ php/update.php
@@ -2,17 +2,22 @@
     if(isset($_POST['update'])){
         echo $_REQUEST['cid'];
         echo $_REQUEST['name'];
+        echo $_REQUEST['date'];
+        echo $_REQUEST['venue'];
+        echo $_REQUEST['participants'];
+        echo $_REQUEST['package'];
+        echo $_REQUEST['remarks'];
         echo "hii";
-        require_once 'db.php';
-        $sql=  "UPDATE orders SET name='".$_POST['name']."', date='".$_POST['date']."', 
-                venue='".$_POST['venue']."', participants='".$_POST['participants']."', package='".$_POST['package']."', 
-                remarks='".$_POST['remarks']."' WHERE customerid=".$_POST['cid'];
+        //require_once 'db.php';
+        // $sql=  "UPDATE orders SET name='".$_POST['name']."', date='".$_POST['date']."', 
+        //         venue='".$_POST['venue']."', participants='".$_POST['participants']."', package='".$_POST['package']."', 
+        //         remarks='".$_POST['remarks']."' WHERE customerid=".$_POST['cid'];
 
-        if ($con->query($sql) === TRUE) {
-            echo("Done");
-        } else {
-            echo "Error: " . $sql . "<br>" . $con->error;
-        }
+        // if ($con->query($sql) === TRUE) {
+        //     echo("Done");
+        // } else {
+        //     echo "Error: " . $sql . "<br>" . $con->error;
+        // }
         //header('Refresh:0');
     }
 
