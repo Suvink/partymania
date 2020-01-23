c6a419433c5396271c81fcba7d51c79d9e0f92dc
Suvink <hello@suvink.me>
2020-01-23 17:40:38 +0530

    Admin Table

--- admin.php
+++ admin.php
@@ -12,8 +12,9 @@
 <?php
     if(isset($_POST['update'])){
         require_once 'db.php';
-        $sql=  "UPDATE orders SET customerid=".$POST['id'].", name=".$POST['name'].", date=".$POST['date'].", 
-                venue=".$POST['venue'].", participants=".$POST['participants'].", package=".$POST['package'].", remarks=".$POST['remarks'].") ";
+        $sql=  "UPDATE orders SET name=".$POST['name'].", date=".$POST['date'].", 
+                venue=".$POST['venue'].", participants=".$POST['participants'].", package=".$POST['package'].", 
+                remarks=".$POST['remarks']."WHERE customerid=".$POST['id'];
 
         if ($con->query($sql) === TRUE) {
             echo("Done");
