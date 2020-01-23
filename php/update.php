4fb4537b4f171c1e72685b254e9f97cffcdcafab
Suvink <hello@suvink.me>
2020-01-23 18:19:23 +0530

    Admin Table

--- php/update.php
+++ php/update.php
@@ -8,16 +8,16 @@
         echo $_REQUEST['package'];
         echo $_REQUEST['remarks'];
         echo "hii";
-        //require_once 'db.php';
-        // $sql=  "UPDATE orders SET name='".$_POST['name']."', date='".$_POST['date']."', 
-        //         venue='".$_POST['venue']."', participants='".$_POST['participants']."', package='".$_POST['package']."', 
-        //         remarks='".$_POST['remarks']."' WHERE customerid=".$_POST['cid'];
+        require_once 'db.php';
+        $sql=  "UPDATE orders SET name='".$_REQUEST['name']."', date='".$_REQUEST['date']."', 
+                venue='".$_REQUEST['venue']."', participants='".$_REQUEST['participants']."', package='".$_REQUEST['package']."', 
+                remarks='".$_REQUEST['remarks']."' WHERE customerid=".$_REQUEST['customer_id'];
 
-        // if ($con->query($sql) === TRUE) {
-        //     echo("Done");
-        // } else {
-        //     echo "Error: " . $sql . "<br>" . $con->error;
-        // }
+        if ($con->query($sql) === TRUE) {
+            echo("Done");
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
         //header('Refresh:0');
     }
 
