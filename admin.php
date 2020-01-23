11faad2692c6ad9bc51de26d97f8160dfbf782a7
Suvink <hello@suvink.me>
2020-01-23 18:21:55 +0530

    Admin Table

--- admin.php
+++ admin.php
@@ -9,29 +9,7 @@
     // }
 ?>
 
-<?php
-    if(isset($_POST['update'])){
-        echo $_REQUEST['customer_id'];
-        echo $_REQUEST['name'];
-        echo $_REQUEST['date'];
-        echo $_REQUEST['venue'];
-        echo $_REQUEST['participants'];
-        echo $_REQUEST['package'];
-        echo $_REQUEST['remarks'];
-        echo "hii";
-        require_once 'db.php';
-        $sql=  "UPDATE orders SET name='".$_REQUEST['name']."', date='".$_REQUEST['date']."', 
-                venue='".$_REQUEST['venue']."', participants='".$_REQUEST['participants']."', package='".$_REQUEST['package']."', 
-                remarks='".$_REQUEST['remarks']."' WHERE customerid=".$_REQUEST['customer_id'];
 
-        if ($con->query($sql) === TRUE) {
-            echo("Done");
-        } else {
-            echo "Error: " . $sql . "<br>" . $con->error;
-        }
-        header('Refresh:0');
-    }
-?>
 
 
 
@@ -157,7 +135,7 @@
                         //Edit Table
                         echo '
                         <tr>
-                            <form action="admin.php" method="POST">
+                            <form action="php/update.php" method="POST">
                                 <td><input class="adminInput" type="text" name="customer_id" value="'.$row["customerid"].'" readonly></td>
                                 <td><input class="adminInput" type="text" name="name" value="'.$row["name"].'"></td>
                                 <td><input class="adminInput" type="date" name="date" value="'.$row["date"].'"></td>
@@ -182,7 +160,7 @@
                 else{
                     echo "No results";
                 }
-            //$con-> close();
+            $con-> close();
             
             ?>
             
