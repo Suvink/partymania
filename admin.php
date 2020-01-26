7c0d44e0fb40f4743251d6153c58edfe9a0b1a68
Suvink <hello@suvink.me>
2020-01-26 14:04:52 +0530

    Added pdf generation

--- admin.php
+++ admin.php
@@ -21,6 +21,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" 
             crossorigin="anonymous">
     </script>
+     <script src="js/script.js">
 </head>
 
 <body>
@@ -199,5 +200,5 @@
     </div>
     <!-- JSPDF Library -->
     
-    <script src="js/script.js">
+   
 </body>
