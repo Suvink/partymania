55f45c1d7d113cc77816edbd2e7453ddac6a37c3
Suvink <hello@suvink.me>
2020-01-26 11:44:37 +0530

    Minor changes

--- add.php
+++ add.php
@@ -19,6 +19,8 @@
     <script>
         let today = new Date().toISOString().split('T')[0];
         document.getElementsByName("cdate")[0].attr('min', today);
+        console.log(today);
+        console.log(document.getElementsByName("cdate")[0].min;);
     </script>
 </head>
 
